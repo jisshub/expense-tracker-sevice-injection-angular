49e4e19b78f1a6346a692900678af5b639f71d96
jisshub <jissmon476@gmail.com>
2020-05-07 20:16:34 +0530

    injected expense service to the appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
+import { ExpenseService } from './expense.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // provide the instance of ExpenseService 
+  providers: [ExpenseService]
 })
 export class AppComponent {
   title = 'expense-tracker-app';
 
+  // define a constructor to let angular v need an instance of ExoenseServices
+  constructor(private xpenseService: ExpenseService){}
+
   expenseList: {description: string, date: string, amount: string}[] = [];
 
-  onFetchExpense(accessData: {description: string, date: string, amount: string} ){
-      this.expenseList.push({
-        description: accessData.description,
-        date: accessData.date,
-        amount: accessData.amount
-      })
-    console.log(this.expenseList);    
-  }
+  // onFetchExpense(accessData: {description: string, date: string, amount: string} ){
+  //     this.expenseList.push({
+  //       description: accessData.description,
+  //       date: accessData.date,
+  //       amount: accessData.amount
+  //     })
+  //   console.log(this.expenseList);    
+  // }
   
 }
--- src/app/expense.service.ts
+++ src/app/expense.service.ts
@@ -1,3 +1,13 @@
 export class ExpenseService{
-    
+    // create a method and give 3 parameters
+    expenseList: Array<{description: string, date: string, amount: string}>
+
+    // push an object to the array
+    addExpenses(description: string, date: string, amount: string){ 
+        this.expenseList.push({
+            description: description,
+            date: date,
+            amount: amount
+        })
+    }
 }
