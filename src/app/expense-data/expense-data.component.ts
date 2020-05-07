c24cf16f84b564dbebcd8bc9a42fa7bd8f7b8182
jisshub <jissmon476@gmail.com>
2020-05-07 20:34:09 +0530

    invoke addExpense method - passed arguments

--- src/app/expense-data/expense-data.component.ts
+++ src/app/expense-data/expense-data.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ExpenseService } from '../expense.service';
 
 @Component({
   selector: 'app-expense-data',
@@ -12,13 +13,20 @@ export class ExpenseDataComponent implements OnInit {
   @ViewChild("expenseAmount", {static: true}) getAmount: ElementRef;
 
   // @Output() throwExpenses = new EventEmitter<{description: string, date: string, amount: string}>();
-  constructor() { }
+  constructor(private xpenseService: ExpenseService) { }
 
   ngOnInit(): void {
 
   }
 
   onSubmit(){
+
+    // invoke the addExpense() - pass the arguments as string.
+    // here v pass of each inputs
+    this.xpenseService.addExpenses(this.getDesc.nativeElement.value, 
+                                   this.getDate.nativeElement.value,
+                                   this.getAmount.nativeElement.value);
+
     // this.throwExpenses.emit({description: this.getDesc.nativeElement.value, date: this.getDate.nativeElement.value, amount: this.getAmount.nativeElement.value});
   }
 
